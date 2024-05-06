ca2dff67160e431bd16da4069acc52e4ae418079
Vinicius Lelé <vinicius.a.lele@hotmail.com>
2024-05-05 22:55:15 -0300

    step3: file indentation

--- nivel3/lista_combo_cidades.php
+++ nivel3/lista_combo_cidades.php
@@ -1,16 +1,15 @@
 <?php
 
-function lista_combo_cidades($id_cidade){
-    $conn = mysqli_connect("localhost","root","root","brasil");
-    $output ='';
+function lista_combo_cidades($id_cidade)
+{
+    $conn = mysqli_connect("localhost", "root", "root", "brasil");
+    $output = '';
     $result = mysqli_query($conn, 'SELECT * FROM cidade ORDER BY nome');
-    if($result)
-    {
-        while ($row = mysqli_fetch_assoc($result))
-        {
+    if ($result) {
+        while ($row = mysqli_fetch_assoc($result)) {
             $id = $row['id'];
             $nome = $row['nome'];
-            $check = ($id == $id_cidade)?'selected=1':'';
+            $check = ($id == $id_cidade) ? 'selected=1' : '';
             $output .= "<option {$check} value='{$id}'> $nome </option>";
         }
     }
