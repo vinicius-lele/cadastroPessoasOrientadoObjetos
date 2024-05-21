6d5e1e415a1187982471be11a15b4dac323a598b
Vini Lelé <vinicius.a.lele@hotmail.com>
2024-05-21 15:55:14 -0300

    namespaces

---
+++ modulo6/App/Model/Cidade.php
@@ -0,0 +1,7 @@
+<?php
+use Livro\Database\Record;
+
+class Cidade extends Record
+{
+    const TABLENAME = 'cidade';
+}
---
+++ modulo6/App/Model/Pessoa.php
@@ -0,0 +1,7 @@
+<?php
+use Livro\Database\Record;
+
+class Pessoa extends Record
+{
+const TABLENAME = 'pessoa';
+}
